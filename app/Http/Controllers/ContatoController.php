0a649740a7446a1d94c20d38018e82728bc9586f
Luan Batista <luanbatista2007@gmail.com>
2023-01-28 20:14:30 -0300

    Customizando mensagens de erro (traduzindo)

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -38,14 +38,27 @@ class ContatoController extends Controller
         //REALIZAR VALIDAÇÃO DOS DADOS RECEBIDOS NO REQUEST
         //AS CHAVES SÃO OS NAMES DOS INPUTS
 
-        $request->validate([
+        $regras = [
             //VALIDAÇÃO UNIQUE: PARÂMENTRO DEVE CONTER NOME DA TABELA NO BANCO COM O CHAVE/NAME
-            'nome' => 'required|min:3|max:40|unique:site_contatos', //NOMES COM MÍNIMO 3 CARACTERES E MÁXIMO 40
+            'nome' => 'required|min:3|max:40|unique:site_contatos',
+            //NOMES COM MÍNIMO 3 CARACTERES E MÁXIMO 40
             'telefone' => 'required',
             'email' => 'email',
             'motivo_contatos_id' => 'required',
-            'mensagem' => 'required',
-        ]);
+            'mensagem' => 'required|max:2000'
+        ];
+
+        $feedback = [
+            'nome.min' => 'O campo nome precisa ter no mínimo 3 catacteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'nome.unique' => 'O nome informado já está em uso',
+            'email' => 'O :attribute informado não é válido',
+
+            'required' => 'O campo :attribute é obrigatório',
+            'mensagem.max' => 'A mensagem deve ter no máximo 2000'
+        ];
+
+        $request->validate($regras, $feedback);
 
         SiteContato::create($request->all());
         return redirect()->route('site.index');
