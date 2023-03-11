329e5b1e8dbe1ea288a163cc1f47e62e734725d1
Luan Batista <31670117+Luan-Btta@users.noreply.github.com>
2023-03-11 19:11:09 -0300

    Refactor: Produto em Prod. Detalhes usando BelongsTo

--- app/Models/ProdutoDetalhe.php
+++ app/Models/ProdutoDetalhe.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use App\Models\Produto;
 
 class ProdutoDetalhe extends Model
 {
     use HasFactory;
     protected $fillable = ['produto_id', 'comprimento', 'largura', 'altura', 'unidade_id'];
+
+    public function Produto() :BelongsTo {
+        return $this->BelongsTo(Produto::class);
+    }
 }
--- resources/views/app/produto_detalhe/_components/form_create_edit.blade.php
+++ resources/views/app/produto_detalhe/_components/form_create_edit.blade.php
@@ -13,10 +13,11 @@
         <option value="">Selecione o produto referenciado</option>
         @foreach ($produtos as $produto)
             <option value="{{ $produto->id }}"
-                {{ (isset($produtoDetalhe->id) && $produtoDetalhe->produto_id == $produto->id) || $produto->id == old('produto_id') ? 'Selected' : '' }}>
+                {{ (isset($produtoDetalhe->produto->id) && $produtoDetalhe->produto->id == $produto->id) || $produtoDetalhe->produto->id == old('produto_id') ? 'Selected' : '' }}>
                 {{ $produto->nome }}</option>
         @endforeach
     </select>
+    <span>{{ $produtoDetalhe->produto->descricao }}</span>
     {{ $errors->has('produto_id') ? $errors->first('produto_id') : '' }}
 
     <input type="text" name="comprimento" value="{{ $produtoDetalhe->comprimento ?? old('comprimento') }}"
